8c743cf58990033a50bbc93503b3f214a1f961f1
Alessandro <alessandro.bado22@gmail.com>
2025-03-09 18:29:35 +0100

    Creation of grpah forlder and graph.py file. This file implements 3D graphs and cells count graph

---
+++ codes/cpp/graph_codes/graph.py
@@ -0,0 +1,175 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def get_intervals(num_hour, divisor):
+    intervals = [(i * num_hour) // divisor for i in range(divisor + 1)]
+    return intervals
+
+def plot_3d(xsize, ysize, zsize, intervals, path_in, dir_out):
+    for i in intervals:
+    # for i, file_data_names in enumerate(os.listdir(path_in)):
+        for file_data_name in os.listdir(path_in):
+            if "t"+ str(i) + "_" in file_data_name: 
+                print(file_data_name)
+        
+                # Leggo il file
+                data = np.loadtxt(os.path.join(path_in, file_data_name), comments='#') 
+
+        
+                fig = plt.figure()
+                plot3d = fig.add_subplot(111, projection='3d')
+
+                plot3d.set_title('Cell proliferation at t = ' + str(i))
+                plot3d.set_xlabel('X')
+                plot3d.set_ylabel('Y')
+                plot3d.set_zlabel('Z')
+                plot3d.set_xlim([0, xsize])
+                plot3d.set_ylim([0, ysize])
+                plot3d.set_zlim([0, zsize])
+
+
+                # Impostare i limiti degli assi
+                plot3d.set_xlim([0, xsize])
+                plot3d.set_ylim([0, ysize])
+                plot3d.set_zlim([0, zsize])
+
+                # Genera i valori dei tick a multipli di 5
+                x_ticks = np.arange(0, xsize+1, 5)
+                y_ticks = np.arange(0, ysize+1, 5)
+                z_ticks = np.arange(0, zsize+1, 5)
+
+                # Imposta i tick sugli assi
+                plot3d.set_xticks(x_ticks)
+                plot3d.set_yticks(y_ticks)
+                plot3d.set_zticks(z_ticks)
+
+                # Formatto i tick degli assi come interi
+                plot3d.xaxis.set_major_formatter(plt.FormatStrFormatter('%d'))
+                plot3d.yaxis.set_major_formatter(plt.FormatStrFormatter('%d'))
+                plot3d.zaxis.set_major_formatter(plt.FormatStrFormatter('%d'))
+
+
+                # STAMPO SIA HEALTHY CHE CANCER CELLS
+                #_ = [
+                #    plot3d.bar3d(
+                #        data[j][0], data[j][1], data[j][2],
+                #        1, 1, 1,
+                #        color=(0, 1, 0) if data[j][9] == 1 else (1, 0, 0),
+                #        alpha=0.05 if data[j][9] == 1 else 1
+                #    )
+                #    for j in range(len(data)) if data[j][9] in [1, -1]
+                #]
+
+                # STAMPO SOLO CANCER
+                _ = [
+                    plot3d.bar3d(
+                        data[j][0], data[j][1], data[j][2],
+                        1, 1, 1,
+                        color=(1, 0, 0),  # rosso
+                        alpha=1
+                    )
+                    for j in range(len(data)) if data[j][9] == -1
+                ]
+        
+                # Salvare il grafico come immagine nella cartella di output
+                output_path = os.path.join(dir_out, f't{i}_gd_3d.png')
+                plt.savefig(output_path)
+                plt.close()
+
+def plot_2d(xsize, ysize, zsize, layers, intervals, path_in, dir_out):
+    # Itero su tutti i file (diversi valori di tick_list)
+    for i, file_data_names in enumerate(os.listdir(path_in)):
+
+        # Leggo il file
+        data = np.loadtxt(os.path.join(path_in, file_data_names), comments='#')
+
+        for layer in layers:
+
+            # La prima colonna contiene il layer. Siccome ce ne possono sono diversi
+            # seleziono solo quelli corrispondente al layer delll'iterazuione
+            # Esegui lo slicing per mantenere solo le righe in cui il valore della terza colonna è uguale a layer
+            filtered_data = data[data[:, 2] == layer]
+
+            # Grafico della cell_proliferation:
+            fig, axs = plt.subplots(2, 2, constrained_layout=True)
+            fig.suptitle('Cell proliferation at t = ' + str(intervals[i])
+            + ' for layer = ' + str(layer))
+
+            # Set titles
+            titles = ['Type of cells', 'Cell density', 'Glucose density', 'Oxygen density']
+            for ax, title in zip(axs.flat, titles):
+                ax.set(title=title)
+
+            # MODIFICA
+
+            # Salvo i grafici
+            output_path = os.path.join(dir_out, 
+            f't{intervals[i]}_gd_2d.png')
+            plt.savefig(output_path)
+            plt.close()
+
+def create_matrix(xsize, ysize, data):
+
+    # Correggiamo la creazione della matrice dei colori per essere tridimensionale
+    color_matrix = np.zeros((xsize, ysize, 3), dtype=float)
+    density_matrix = np.empty((xsize, ysize), dtype=float)
+    glucose_matrix = np.zeros((xsize, ysize), dtype=float)
+    oxygen_matrix = np.empty((xsize, ysize), dtype=float)
+
+
+# Riempie la matrice dei colori con i valori RGB per le coordinate corrispondenti
+    for row in data:
+        x, y = int(row[0]), int(row[1])
+        r, g, b = row[3:6]
+        color_matrix[x, y] = [r / 255.0, g / 255.0, b / 255.0]  # Normalizza i valori RGB da 0-255 a 0-1
+        density_matrix[x,y] =  row[3]
+        glucose_matrix[x,y] = row[7]
+        oxygen_matrix[x,y] = row[8]
+
+    # return color_matrix , density_matrix
+    return color_matrix, density_matrix, glucose_matrix, oxygen_matrix
+def cells_num(file_name, path_in, path_out):
+    # Leggo il file
+    data = np.loadtxt(os.path.join(path_in, file_name), comments='#')
+    data = data.T
+
+    fig, ax = plt.subplots()
+    # Disegno le somme. plt.plot(x, y)
+    plt.plot(data[0], data[1] + data[2]+ data[3], "k.-", label="Total Cells", alpha=0.7)
+    plt.plot(data[0], data[1], "g.-", label="Healthy Cells", alpha=0.7)
+    plt.plot(data[0], data[2], "r.-", label="Cancer Cells", alpha=0.7)
+    plt.plot(data[0], data[3], "y.-", label="OAR Cells", alpha=0.1)
+
+
+    plt.yscale('log')
+
+    plt.xlabel('Hours')
+    plt.ylabel('Cell sum')
+    plt.title('Cell count')
+    plt.legend()
+    plt.grid()
+
+    # Salvo i grafici
+    output_path = os.path.join(path_out, f'cells_sum.png')
+    plt.savefig(output_path)
+    plt.close()
+
+
+
+path_in_tab  = "/home/ale/Documenti/programming/working/results/data/tabs/"
+path_in_num  = "/home/ale/Documenti/programming/working/results/data/cell_num/"
+dir_out_sum = "/home/ale/Documenti/programming/working/results/graphs/"
+dir_out_3d = "/home/ale/Documenti/programming/working/results/graphs/3d"
+dir_out_2d = "/home/ale/Documenti/programming/working/results/graphs/2d"
+xsize = 21
+ysize = 21
+zsize = 21
+num_hour = 400
+divisor = 4
+
+intervals = get_intervals(num_hour, divisor)
+print(intervals)
+plot_3d(xsize, ysize, zsize, intervals, path_in_tab, dir_out_3d) 
+cells_num("cell_counts.txt", path_in_num, dir_out_sum)
+print(os.getcwd())
