b2f87a18d77294f3a4666d27d5ef91fa68961209
Alessandro <alessandro.bado22@gmail.com>
2025-03-09 23:56:20 +0100

    folder managment

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ale/Documenti/programming/proj_radio_rein/cpp/src/cell.cpp" "CMakeFiles/test.dir/src/cell.cpp.o" "gcc" "CMakeFiles/test.dir/src/cell.cpp.o.d"
+  "/home/ale/Documenti/programming/proj_radio_rein/cpp/src/controller_3d.cpp" "CMakeFiles/test.dir/src/controller_3d.cpp.o" "gcc" "CMakeFiles/test.dir/src/controller_3d.cpp.o.d"
+  "/home/ale/Documenti/programming/proj_radio_rein/cpp/src/grid_3d.cpp" "CMakeFiles/test.dir/src/grid_3d.cpp.o" "gcc" "CMakeFiles/test.dir/src/grid_3d.cpp.o.d"
+  "/home/ale/Documenti/programming/proj_radio_rein/cpp/src/test.cpp" "CMakeFiles/test.dir/src/test.cpp.o" "gcc" "CMakeFiles/test.dir/src/test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/cell.cpp.o"
+  "CMakeFiles/test.dir/src/cell.cpp.o.d"
+  "CMakeFiles/test.dir/src/controller_3d.cpp.o"
+  "CMakeFiles/test.dir/src/controller_3d.cpp.o.d"
+  "CMakeFiles/test.dir/src/grid_3d.cpp.o"
+  "CMakeFiles/test.dir/src/grid_3d.cpp.o.d"
+  "CMakeFiles/test.dir/src/test.cpp.o"
+  "CMakeFiles/test.dir/src/test.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
