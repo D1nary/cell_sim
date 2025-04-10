2a50c86be26dd7259cb9e0aa54fa2aff23f32e2f
Alessandro <alessandro.bado22@gmail.com>
2025-04-10 11:38:56 +0200

    Rename parent directory

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/cell.cpp.o"
+  "CMakeFiles/main.dir/src/cell.cpp.o.d"
+  "CMakeFiles/main.dir/src/controller_3d.cpp.o"
+  "CMakeFiles/main.dir/src/controller_3d.cpp.o.d"
+  "CMakeFiles/main.dir/src/grid_3d.cpp.o"
+  "CMakeFiles/main.dir/src/grid_3d.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
