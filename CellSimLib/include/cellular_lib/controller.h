b680e7acc47f5976989d7e9d0dc4382658c81e43
Alessandro <alessandro.bado22@gmail.com>
2025-08-04 18:57:21 +0200

    Rename src files and paste the old version of the files in order to create and test the library correctly

--- CellSimLib/include/cellular_lib/controller.h
+++ CellSimLib/include/cellular_lib/controller.h
@@ -11,9 +11,9 @@
 class Controller {
 public:
 
-    Controller(int xsize, int ysize, int zsize, int sources_num,
-        double cradius, double hradius, int hcells, int ccells,
-        int num_hour, const std::vector<int>& intervals);
+    Controller(int xsize, int ysize, int zsize, int sources_num, int* intervals);
+    Controller(int hcells, int xsize, int ysize, int zsize, int sources_num,
+                int x1, int x2, int y1, int y2, int z1, int z2);
     ~Controller();
 
     int*** grid_creation(double hradius, double cradius);
@@ -36,10 +36,10 @@ public:
     double get_center_x();
     double get_center_y();
     double get_center_z();
-    std::vector<int> get_intervals(int num_hour, int divisor);
+    int* get_intervals(int num_hour, int divisor);
 
     void saveDataTab(const std::string &path, const std::vector<std::string>& filenames, 
-        const std::vector<int>& intervals, int intervalsSize);
+                    int* intervals, int intervalsSize);
     void saveCellCounts(const std::string &path, const std::string &filename);
     void createDirectories(const std::vector<std::string>& paths);
     void tempCellCounts();
@@ -48,22 +48,17 @@ public:
     void clear_tempDataTab();
     void printIntervals(int divisor, int* intervals);
 
-    void test_treatment(int week, int rad_days, int rest_days, double dose);
-    std::vector<int> get_cell_counts() const;
-
-    Grid* growth();
+    void treatment(int week, int rad_days, int rest_days, int dose);
 
 
 private:
     bool self_grid;
     Grid * grid;
     OARZone * oar;
+    int* intervals;
     int* intervals_sum;
     std::vector<std::vector<int>> tempCounts;
     std::vector<std::vector<double>> tempDataTabMatrix;
-
-    int num_hour;
-    std::vector<int> intervals;
 };
 
 
